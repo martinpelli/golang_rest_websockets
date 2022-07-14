88c0504734637026601c0a44d84a39dac487c1d8
Pelli <martinpelli@outlook.com>
2022-07-14 09:33:51 -0300

    repository

---
+++ repositorys/user.go
@@ -0,0 +1,25 @@
+package repositorys
+
+import (
+	"context"
+	"golang_rest_websockets/models"
+)
+
+type UserRepository interface {
+	InsertUser(context context.Context, user *models.User) error
+	GetUserById(context context.Context, id int64) (*models.User, error)
+}
+
+var implementation UserRepository
+
+func SetRepository(repository UserRepository) {
+	implementation = repository
+}
+
+func InsertUser(context context.Context, user *models.User) error {
+	return implementation.InsertUser(context, user)
+}
+
+func GetUserById(context context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserById(context, id)
+}
